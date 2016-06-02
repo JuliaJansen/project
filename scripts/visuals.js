72735b8fb5119e7fb5a0389d1d83133ee7abc6e0
JuliaJansen <juliartjansen@gmail.com>
2016-06-02 21:24:12 +0200

    update
    voila

--- scripts/visuals.js
+++ scripts/visuals.js
@@ -5,8 +5,10 @@
  * Loading data and preparing visualisations
  */
 
-var slider = d3.slider().min(2004).max(2015).showRange(true).value(2008);
-d3.select('#slider').call(slider);
+window.onload = function() {
+	var slider = d3.slider().min(2004).max(2015).showRange(true).value(2008);
+	d3.select('#slider').call(slider);
+}
 
 function parallelGraph() {
 
