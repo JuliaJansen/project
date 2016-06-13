c5825d409237bbea766c5ee7dd517848840d7d1a
JuliaJansen <juliartjansen@gmail.com>
2016-06-13 16:48:31 +0200

    UPDATE
    werkt nog niet....

--- scripts/wastescript.py
+++ scripts/wastescript.py
@@ -17,8 +17,10 @@ with open('../data/WSECTOR_02062016152910695.csv', 'r') as infile:
 		# i = 0
 		# writer.writerow(["TIME", "GEO", "Agriculture", "Mining", "Manufacturing", "Energy_Production", "Water_management", "Construction", "Other"])
 		# read and write
-		for row in reader:
-			if row[3] == "Agriculture forestry and fishing" or row[3] == "Mining and Quarrying" or row[3] == "Manufacturing industries: Total" or row[3] == "Energy Production " or row[3] == "Water supply sewerage waste management etc.." or row[3] == "Construction" or row[3] == "Other":
-				writer.writerow([row[4], row[1], row[3], row[12]])
+		for i, row in enumerate(reader):
+			if row[3] == "Agriculture forestry and fishing " or row[3] == "Mining and Quarrying" or row[3] == "Manufacturing industries: Total" or row[3] == "Energy Production " or row[3] == "Water supply sewerage waste management " or row[3] == "Construction" or row[3] == "Other":
+				print row[3], row[1], row[4]
+			# if row[3] == "Agriculture forestry and fishing " or row[3] == "Mining and Quarrying" or row[3] == "Manufacturing industries: Total" or row[3] == "Energy Production " or row[3] == "Water supply sewerage waste management " or row[3] == "Construction" or row[3] == "Other":
+			# 	writer.writerow([row[4], row[1], row[3], row[12]])
 			
 			
